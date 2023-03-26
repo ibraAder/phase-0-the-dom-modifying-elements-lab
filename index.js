9d8ead9b1a72b5256084024d44f48977f92064da
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-03-26 16:59:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+const main=document.querySelector("#main");
+main.remove();
+
+const newHeader=document.createElement("h1");
+document.body.append(newHeader);
+newHeader.innerHTML="victory <p>Ader is The Champion</p>";
+
+newHeader.setAttribute("id", "victory");
+
+ 
+
+
+
+
+
+
+
